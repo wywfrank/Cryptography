de44f65e1010bccf5dd9225657c99cd445477591
Frank Wan <frank@Franks-MacBook-Pro.local>
2020-11-14 00:46:50 -0600

    login

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class login(Resource):
 		data = request.get_json()
 		# TODO: Implement login functionality
 		body=json.loads(data)
-		print "body is "+body
+		print body
 		
 		# TODO: Verify the signed statement.
 		# 	Response format for success and failure are given below. The same
