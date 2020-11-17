21722f0c543b1100bffc954cf44b0153818cb751
Frank Wan <frank@Franks-MacBook-Pro.local>
2020-11-16 19:46:23 -0600

    checkin

--- server.py
+++ server.py
@@ -133,17 +133,47 @@ def create_connection(db_file):
 	conn = None
 	try:
 		conn=sqlite3.connect(db_file)
-		print sqlite3.version
+		return conn
 	except Error as e:
 		print e
-	finally:
-		if conn:
-			conn.close()
-			
+	return conn
+	# finally:
+	# 	if conn:
+	# 		conn.close()
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print e
+	
+
 def main():
+	database = r"pythonsqlite.db"
+
+    sql_create_AUTH_table = """ CREATE TABLE IF NOT EXISTS AUTH (
+                                        did integer PRIMARY KEY,
+                                        owner text,
+                                        security_flag integer NOT NULL
+                                    ); """
+
+	conn = create_connection(database)
+	if conn is not None:
+        create_table(conn, sql_create_AUTH_table)
+	
+    else:
+        print("Error! Cannot create the database connection.")
+
 	secure_shared_service.run(debug=True)
 
 
 if __name__ == '__main__':
-	create_connection(r"pythonsqlite.db")
+	
+
 	main()
