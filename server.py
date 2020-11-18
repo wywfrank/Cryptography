7cf0597a486d268a476d69216ba7491d4966242c
Frank Wan <frank@Franks-MacBook-Pro.local>
2020-11-18 02:35:24 -0600

    checkin

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
-from Crypto import Random
 import hashlib
 import json
 import base64
@@ -15,6 +14,7 @@ import sqlite3
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
+import Random
 
 
 secure_shared_service = Flask(__name__)
