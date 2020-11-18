db61c0749a3060113bff96b4024850179781e6fe
Frank Wan <frank@Franks-MacBook-Pro.local>
2020-11-18 02:53:39 -0600

    checkin

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from Crypto.Cipher import AES
 import hashlib
 import json
 import base64
+from base64 import b64encode, b64decode
 from uuid import uuid4
 import sqlite3
 # from sqlite3 import Error
