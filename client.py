1a3ecc491bc30b25b5eb943fc6ff7eade5f2105e
Frank Wan <frank@Franks-MacBook-Pro.local>
2020-11-20 02:22:17 -0600

    checkin

--- client.py
+++ client.py
@@ -117,10 +117,10 @@ def grant():
 		 - time duration (in seconds) for which acess is granted
 		Send request to server with required parameters (action = 'grant') using post_request()
 	'''
-	a = datetime.datetime.now().time()
+	a = datetime.datetime(100,1,1,11,34,59).now().time()
 	b = a + datetime.timedelta(seconds=300) # days, seconds, then other fields.
-	print(a.time())
-	print(b.time())
+	print a.time()
+	print b.time()
 	return
 
 def delete():
